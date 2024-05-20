65369911445f26619f10761a7ec50ca7f7f34530
DanRoscigno <dan@roscigno.com>
2024-05-20 17:35:48 -0400

    correct edit URL
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/danroscigno/Recipes/tree/main/website/recipes/',
+            'https://github.com/danroscigno/Recipes/tree/main/website/',
         },
         theme: {
           customCss: './src/css/custom.css',
